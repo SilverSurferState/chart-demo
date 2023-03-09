88df1cf7799bde2b730429ac1b380565e12e9eda
One16-UR <umberto.righetti@one16.be>
2023-03-09 23:57:31 +0100

    demo app voor hybrid

--- App.js
+++ App.js
@@ -1,13 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Pressable, TextInput} from 'react-native';
+import { LineChart } from "react-native-chart-kit";
+import React, { useState } from 'react';
+
 
 export default function App() {
-  return (
+  const [data, setData] = useState([80, 75, 90, 95, 75, 30]);
+  const [number, setNumber] = useState(0);
+
+  return <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LineChart
+    data={{
+      labels: ["Johannes", "Toon", "Floo", "Ward", "Fouad", "Umberto"],
+      datasets: [
+        {
+          data: data,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        },
+        {
+          data: [
+            10, 20, 40, 10, 30, 60
+          ]
+        }
+
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel=""
+    yAxisSuffix="%"
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 0,
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+  />
     </View>
-  );
+    <View style={styles.container}>
+    <TextInput
+        style={styles.input}
+        onChangeText={setNumber}
+        value={Number}
+        placeholder="Voer een cijfer in"
+      />
+    </View>
+    <View>
+    <Pressable
+      style={({ pressed }) => [
+        styles.button,
+        {
+          backgroundColor: pressed ? '#6c757d' : '#007bff',
+        },
+      ]}
+      onPress={() => setData([...data, number ])}
+    >
+      <Text style={styles.text}>Voeg toe!</Text>
+    </Pressable>
+    </View>
+    </>
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +77,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    borderRadius: 5,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  container: {
+    margin: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
   },
 });
